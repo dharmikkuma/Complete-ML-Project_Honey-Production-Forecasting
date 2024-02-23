ddfe8cc449d9fdf52bc79412649585c12c92d3ce
Dharmikkumar Anaghan <98855357+dharmikkuma@users.noreply.github.com>
2024-02-23 15:40:08 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from Project import logger
+from Project.pipeline._01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
